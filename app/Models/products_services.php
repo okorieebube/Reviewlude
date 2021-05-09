c3f6f58952cb76a59008d20e54a2de64d85a5c51
Ebube Okorie <okorieebube1@gmail.com>
2021-05-09 15:09:35 +0100

    feat(password): users can update their password

--- app/Models/products_services.php
+++ app/Models/products_services.php
@@ -21,15 +21,6 @@ class products_services extends Model
      * @var array
      */
 
-
-    // $table->string('unique_id')->unique();
-    // $table->string('user_id')->nullable();
-    // $table->string('name')->nullable();
-    // $table->string('total_reviews')->nullable();
-    // $table->string('score')->nullable();
-    // $table->string('tags')->nullable();
-    // $table->string('performance')->nullable();
-
     protected $fillable = [
         'unique_id',
         'user_id',
@@ -43,23 +34,21 @@ class products_services extends Model
 
     public function getSingle($condition){
 
-        $users = products_services::where($condition)->first();
+        $products_services = products_services::where($condition)->first();
 
-        return $users;
+        return $products_services;
 
     }
 
     function updateProductServices($requestObject){
 
-        $business_settings = business_settings::find($requestObject->id);
-        $business_settings->description = $requestObject->description ?? $business_settings->description;
-        $business_settings->street_address = $requestObject->street_address ?? $business_settings->street_address;
-        $business_settings->zip_code = $requestObject->zip_code ?? $business_settings->zip_code;
-        $business_settings->city = $requestObject->city ?? $business_settings->city;
-        $business_settings->country = $requestObject->country ?? $business_settings->country;
-        $business_settings->deleted_at = $requestObject->deleted_at ?? $business_settings->deleted_at;
-        if($business_settings->save()){
-            return $business_settings;
+        $products_services = business_settings::find($requestObject->id);
+        $products_services->total_reviews = $requestObject->total_reviews ?? $products_services->total_reviews;
+        $products_services->score = $requestObject->score ?? $products_services->score;
+        $products_services->tags = $requestObject->tags ?? $products_services->tags;
+        $products_services->performance = $requestObject->performance ?? $products_services->performance;
+        if($products_services->save()){
+            return $products_services;
         }
     }
 }
